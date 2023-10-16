ec44ae5d67506a59dbef089620c9a17a294db2c4
liamg15 <golding.liam@outlook.com>
2023-10-16 11:05:21 -0700

    Analysis as of October 15, 2023

--- weight_analysis.R
+++ weight_analysis.R
@@ -8,11 +8,10 @@ library(lubridate)
 # Load and pre-process data
 wbd <- read.delim(here("weight_data.txt"), sep = " ", header = F, col.names = c("Date", "Time", "Liam")) %>% 
   mutate(Date = as.numeric(as.Date(Date)-19492),
-         Time = as.ITime(x = Time)) %>% 
-  filter(Date < 94)
+         Time = as.ITime(x = Time)) 
 
 # Model weight change by day
-mod <- lm(Liam ~ Date, wbd)
+mod <- lm(Liam ~ Date + Time, wbd)
 summary(mod)
 plot(mod)
 
@@ -25,17 +24,24 @@ pred$Date <- as_date(pred$x+19492)
 wbd$Date <- as_date(wbd$Date+19492)
 
 # Plot weight by day
-#pdf("weight.pdf", width = 4, height = 3.5)
-wbd %>% ggplot(aes(Date, Liam)) +
-  geom_point(fill = "darkblue", pch = 21, size = 2, alpha = 0.75) +
+pdf("weight.pdf", width = 4, height = 3.5)
+wbd %>% mutate(Time = as.numeric(Time),
+               Time = case_when(Time < 43200 ~ 'Morning',
+                                Time < 61200 ~ "Afternoon",
+                                Time < 75600 ~ "Evening",
+                                Time >= 75600 ~ "Night"),
+               Time = factor(Time, c("Morning", "Afternoon", "Evening", "Night"))) %>% 
+  ggplot(aes(Date, Liam)) +
+  geom_ribbon(data = pred, aes(ymin = conf.low, ymax = conf.high), alpha = 0.05) +
+  geom_point(aes(size = Time, fill = Time), pch = 21) +
   geom_line(data = pred, color = "darkred") +
-  geom_ribbon(data = pred, aes(ymin = conf.low, ymax = conf.high), alpha = 0.1) +
   labs(x = "Date", y = "Weight (lbs)") +
-  theme_light() 
-#dev.off()
+  guides(fill = guide_legend(title = "Time"), size = guide_legend(title = "Time")) + 
+  theme_light() +
+  theme(legend.position = "bottom")
+ dev.off()
 
 # Results:
-
-# According to this model I gained 0.009 lbs everyday since May 16, 2023. 
+# According to this model I gained 0.0098 lbs everyday since May 16, 2023. 
 # At that rate, I would reach my goal of 170 lbs in 711 days (~ 2 years).
 # I need to increase calories and muscle growth to reach this goal in a reasonable time frame.
