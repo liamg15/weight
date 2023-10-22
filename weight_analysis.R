4c9f36e07e07da430df85bd232986a6a4e6c0e60
liamg15 <golding.liam@outlook.com>
2023-10-21 19:54:21 -0700

    aesthetic

--- weight_analysis.R
+++ weight_analysis.R
@@ -4,6 +4,7 @@ library(here)
 library(ggeffects)
 library(data.table)
 library(lubridate)
+library(ggsci)
 
 # Load and pre-process data
 wbd <- read.delim(here("weight_data.txt"), sep = " ", header = F, col.names = c("Date", "Time", "Liam")) %>% 
@@ -13,7 +14,6 @@ wbd <- read.delim(here("weight_data.txt"), sep = " ", header = F, col.names = c(
 # Model weight change by day
 mod <- lm(Liam ~ Date + Time, wbd)
 summary(mod)
-plot(mod)
 
 # Predict weight by day from linear model
 pred <- ggpredict(mod, terms = c("Date")) %>% 
@@ -24,19 +24,23 @@ pred$Date <- as_date(pred$x+19492)
 wbd$Date <- as_date(wbd$Date+19492)
 
 # Plot weight by day
-pdf("weight.pdf", width = 4, height = 3.5)
+pdf("weight.pdf", width = 7, height = 4)
 wbd %>% mutate(Time = as.numeric(Time),
-               Time = case_when(Time < 43200 ~ 'Morning',
-                                Time < 61200 ~ "Afternoon",
-                                Time < 75600 ~ "Evening",
-                                Time >= 75600 ~ "Night"),
-               Time = factor(Time, c("Morning", "Afternoon", "Evening", "Night"))) %>% 
+               Time = case_when(Time < 34200 ~ 'Morning (5:30-9:29AM)',
+                                Time < 43200 ~ 'Morning (9:30-11:59AM)',
+                                Time < 54000 ~ "Afternoon (12:30-2:59PM)",
+                                Time < 64800 ~ 'Afternoon (3:00-5:59PM)',
+                                Time < 75600 ~ "Night (6:00-8:59PM)",
+                                Time <= 86400 ~ 'Night (9:00-11:59PM)'),
+               Time = factor(Time, c('Morning (5:30-9:29AM)','Morning (9:30-11:59AM)', "Afternoon (12:30-2:59PM)",'Afternoon (3:00-5:59PM)',"Night (6:00-8:59PM)",'Night (9:00-11:59PM)'))) %>% 
   ggplot(aes(Date, Liam)) +
-  geom_ribbon(data = pred, aes(ymin = conf.low, ymax = conf.high), alpha = 0.05) +
+  geom_line(data = pred, aes(y = conf.low), colour = "grey50", linetype = "dashed") +
+  geom_line(data = pred,aes(y = conf.high), colour = "grey50", linetype = "dashed") +
   geom_point(aes(size = Time, fill = Time), pch = 21) +
   geom_line(data = pred, color = "darkred") +
-  labs(x = "Date", y = "Weight (lbs)") +
-  guides(fill = guide_legend(title = "Time"), size = guide_legend(title = "Time")) + 
+  labs(x = "", y = "Weight (lbs)") +
+  guides(fill = guide_legend(title = ""), size = guide_legend(title = "")) + 
+  scale_fill_jama() +
   theme_light() +
   theme(legend.position = "bottom")
  dev.off()
@@ -45,3 +49,5 @@ wbd %>% mutate(Time = as.numeric(Time),
 # According to this model I gained 0.0098 lbs everyday since May 16, 2023. 
 # At that rate, I would reach my goal of 170 lbs in 711 days (~ 2 years).
 # I need to increase calories and muscle growth to reach this goal in a reasonable time frame.
+ 
+ 
